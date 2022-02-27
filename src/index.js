817be5933bf08db77d0ebaca7cee4d5cde41a46e
AndresPalma <93955080+AndresPalma@users.noreply.github.com>
2022-02-27 17:52:44 -0500

    Actualizacion
    routes config database index auth products user

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import app from "./app";
+import './database';
 
-app.listen(4000);
-console.log('Server Listen on port', 4000)
+app.listen(app.get("port"));
+
+console.log("Server on port", app.get('port'));
